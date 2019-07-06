7b65f803c1625fbe3110ede647484c31a0dbc94e
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-07-06 15:23:49 +0100

    handle no input file

--- file.c
+++ file.c
@@ -6,23 +6,29 @@
 int
 file_ok(const char *const filename)
 {
+	if (!filename)
+	{
+		fprintf(stderr, "Must specify an image file\n");
+		return 0;
+	}
+	else
 	if (access(filename, F_OK) != 0)
-		{
+	{
 			fprintf(stderr, "%s does not exist\n", filename);
 			return 0;
-	  }
+	}
 	else
 	if (access(filename, R_OK) != 0)
-	  {
+	{
 			fprintf(stderr, "Cannot read %s\n", filename);
 			return 0;
-	  }
+	}
 	else
 	if (access(filename, W_OK) != 0)
-	  {
+	{
 			fprintf(stderr, "Cannot write to %s\n", filename);
 			return 0;
-	  }
+	}
 	else
 		return 1;
 }
