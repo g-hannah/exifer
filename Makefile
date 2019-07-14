43193675f36d1e96ac803e3be577daec84bdcce6
gamh86 <kZDmLRwuUIoGibKVGiAixJEur@yandex.ru>
2019-07-14 14:18:25 +0100

    Bump version 0.2.14

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 DEBUG:=0
-BUILD:=0.2.13
+BUILD:=0.2.14
 CC=gcc
 WFLAGS=-Wall -Werror
 CFILES=main.c exif.c file.c logging.c
--- exif.h
+++ exif.h
@@ -7,7 +7,7 @@
 # define _EOL "\n"
 #endif
 
-#define BUILD			"0.2.13"
+#define BUILD			"0.2.14"
 
 #define APP1_MARKER		"\xff\xe1"
 #define END_MARKER		"\xff\xd8"
