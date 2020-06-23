ba0d5a4f0ff53314206a20db43cf533bcd4aa29a
gamh86 <g.amh.86@outlook.fr>
2020-06-23 16:26:20 +0100

    Define GXXBIT_VAL() functions to get values in correct endianness

--- exif.c
+++ exif.c
@@ -16,8 +16,18 @@
 #include "exif.h"
 #include "logging.h"
 
+#define __unused __attribute__((unused))
+
 #define UNIX_EPOCH_DATE	"1970:01:01 00:00:00"
 #define MILLENIUM_DATE	"2000:01:01 00:00:00"
+
+int count = 0;
+
+typedef struct Data_Array
+{
+	datum_t *data;
+	int nr;
+} data_array_t;
 
 /*
  * Indices into the exif_flag_t array
@@ -44,10 +54,10 @@ enum
 	CAMERA_UNIQUE_CAMERA_MODEL,
 	CAMERA_LABEL,
 	CAMERA_BODY_SERIAL,
-	NR_DATA
+	NR_SENSITIVE
 };
 
-static exif_flag_t EXIF_FLAGS[NR_DATA] =
+static exif_flag_t SENSITIVE_DATA[NR_SENSITIVE] =
 {
 	{ "Created", "\x90\x04", TYPE_ASCII },
 	{ "Original", "\x90\x03", TYPE_ASCII },
@@ -71,11 +81,17 @@ static exif_flag_t EXIF_FLAGS[NR_DATA] =
 	{ "Camera Body Serial", "\xa4\x31", TYPE_ASCII }
 };
 
+/*
+ * Part of sensitive data but has to be
+ * extracted separately from the rest
+ * due to how it is parsed.
+ */
 enum
 {	
 	GPS_LATITUDE_LETTER = 0,
 	GPS_LONGITUDE_LETTER,
 	GPS_VERSION_ID,
+	GPS_DATESTAMP,
 	GPS_LATITUDE,
 	GPS_LONGITUDE,
 	GPS_SATELLITES,
@@ -87,6 +103,7 @@ static exif_flag_t GPS_FLAGS[GPS_NR_DATA] =
 	{ "N/S", "\x00\x01", TYPE_ASCII },
 	{ "E/W", "\x00\x03", TYPE_ASCII },
 	{ "GPS Version ID", "\x00\x00", TYPE_BYTE },
+	{ "GPS Datestamp", "\x00\x1d", TYPE_ASCII },
 	{ "GPS Latitude", "\x00\x02", TYPE_RATIONAL },
 	{ "GPS Longitude", "\x00\x04", TYPE_RATIONAL },
 	{ "GPS Satellites", "\x00\x05", TYPE_ASCII }
@@ -95,50 +112,6 @@ static exif_flag_t GPS_FLAGS[GPS_NR_DATA] =
 #define DATA_COL	"\x1b[38;5;88m"
 #define STRIKE_THROUGH	"\x1b[9;02m"
 #define END_COL		"\x1b[m"
-
-static struct sigaction new_act, old_act;
-static sigjmp_buf __sigsegv__;
-
-static void
-sigsegv_handler(int signo)
-{
-	if (signo != SIGSEGV)
-		return;
-	else
-		siglongjmp(__sigsegv__, 1);
-}
-
-static int
-set_signal_handler(void)
-{
-	clear_struct(&new_act);
-	clear_struct(&old_act);
-	sigemptyset(&new_act.sa_mask);
-	new_act.sa_handler = sigsegv_handler;
-	new_act.sa_flags = 0;
-	if (sigaction(SIGSEGV, &new_act, &old_act) < 0)
-		return -1;
-	else
-		return 0;
-}
-
-#define setup_signal_handler() do { \
-	set_signal_handler();																	\
-	if (sigsetjmp(__sigsegv__, 1) != 0)										\
-	{																											\
-		log_error("Caught segmentation fault - exiting");		\
-		exit(EXIT_FAILURE);																	\
-	}																											\
-} while (0)
-
-static void
-restore_signal_handler(void)
-{
-	if (sigaction(SIGSEGV, &old_act, NULL) < 0)
-		log_error("Failed to restore old signal handler for SIGSEGV");
-
-	return;
-}
 
 int
 random_byte(unsigned char *c)
@@ -192,12 +165,6 @@ random_byte(unsigned char *c)
 	return 0;
 }
 
-static void
-wipe_data(file_t *file, datum_t *datum)
-{
-	return;
-}
-
 #define METADATA_LENGTH 12
 /**
  * Zero out the exif data and the meta-data that points
@@ -243,104 +210,148 @@ exif_start(file_t *file)
 	return (void *)p;
 }
 
+/*
+ * The compiler may complain about these reverse functions since
+ * they may not be included in the code by the preprocessor
+ * depending on the endianness of the machine. So used attribute
+ * unused.
+ */
+static uint32_t
+__unused __reverse_bytes32(uint32_t val)
+{
+	char *p = (char *)&val;
+	char t;
+
+	t = p[3];
+	p[3] = p[0];
+	p[0] = t;
+
+	t = p[2];
+	p[2] = p[1];
+	p[1] = t;
+
+	return *((uint32_t *)p);
+}
+
+static uint32_t
+__unused __reverse_bytes16(uint16_t val)
+{
+	uint8_t t;
+	uint8_t *p = (uint8_t *)&val;
+
+	t = p[1];
+	p[1] = p[0];
+	p[0] = t;
+
+	return *((uint16_t *)p);
+}
+
+/**
+ * Return the value in the correct endianness
+ * for the machine we are running on.
+ *
+ * @param val This is the value we extracted from the exif data
+ * @param endian This is the endianness the exif data is encoded in
+ */
+static uint16_t
+G16BIT_VAL(uint16_t val, int endian)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	if (endian)
+	{
+		return ntohs(val);
+	}
+	else
+		return val;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	if (0 == endian)
+	{
+		return __reverse_bytes16(val);
+	}
+	else
+		return val;
+#else
+# error "What kind of machine is this...?"
+#endif
+}
+
+static uint32_t
+G32BIT_VAL(uint32_t val, int endian)
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	if (endian)
+		return ntohl(val);
+	else
+		return val;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	if (0 == endian)
+		return __reverse_bytes32(val);
+	else
+		return val;
+#else
+# error "What kind of machine is this...?"
+#endif
+}
+
 static void *
 get_data_offset(file_t *file, datum_t *dptr, char *str, size_t slen, uint16_t type, int endian)
 {
 	unsigned char *p = NULL;
-	unsigned char *t = NULL;
 
 	assert(file);
 	assert(dptr);
 	assert(str);
-	assert(lim > file->map);
+	assert(lim > file->map); // LIM is global var
 	assert(file->new_end <= file->map_end);
 
 	p = (unsigned char *)exif_start(file);
-	t = (unsigned char *)str;
 
 	clear_struct(dptr);
 
-	for(;;)
-	{
-		while (memcmp(t, p, 2) && p < (unsigned char *)lim)
-			++p;
+	char c1 = str[0];
+	unsigned char *e = (unsigned char *)lim;
 
-		if (p == (unsigned char *)lim)
+	while (1)
+	{
+		p = memchr(p, c1, e - p);
+		if (!p)
 			return NULL;
 
-		uint16_t _type = *((uint16_t *)(p + 2));
+		if (memcmp(p, str, slen))
+		{
+			++p;
+			continue;
+		}
 
-		if (endian)
-			_type = ntohs(_type);
+		if (p == e)
+			return NULL;
+
+		uint16_t _type = G16BIT_VAL(*((uint16_t *)(p + 2)), endian);
 
 		if (_type != type)
 		{
-			p += 2;
+			++p;
 			continue;
 		}
-		else
-			break;
+
+		break;
 	}
 
+/*
+ * Save pointers to the metadata within the file so that if the user
+ * has specified to wipe the data, we can also wipe this metadata.
+ */
 	dptr->tag_p = (void *)p;
 	dptr->type_p = (void *)((unsigned char *)p + 2);
 	dptr->len_p = (void *)((unsigned char *)p + 4);
 	dptr->offset_p = (void *)((unsigned char *)p + 8);
 
-	if (endian)
-	{
-		uint32_t 	offset;
-		uint32_t	len;
-		uint16_t	type;
-		uint8_t		*t = NULL;
-
-		offset = *((uint32_t *)dptr->offset_p);
-		t = (unsigned char *)&offset;
-		if (*t == 0)
-			offset = htonl(offset);
-		dptr->offset = offset;
-
-		len = *((uint32_t *)dptr->len_p);
-		len = ntohl(len);
-		t = (unsigned char *)&len;
-		if (*t == 0)
-			len = htonl(len);
-		dptr->len = len;
-
-		type = *((uint16_t *)dptr->type_p);
-		type = ntohs(type);
-		t = (unsigned char *)&type;
-		if (*t == 0)
-			type = ntohs(type);
-		dptr->type = type;
-		t = NULL;
-	}
-	else
-	{
-		uint32_t 	offset;
-		uint32_t	len;
-		uint16_t	type;
-		uint8_t		*t = NULL;
-
-		offset = *((uint32_t *)dptr->offset_p);
-		t = (unsigned char *)&offset;
-		if (*t == 0)
-			offset = ntohl(offset);
-		dptr->offset = offset;
-
-		len = *((uint32_t *)dptr->len_p);
-		t = (unsigned char *)&len;
-		if (*t == 0)
-			len = ntohl(len);
-		dptr->len = len;
-
-		type = *((uint16_t *)dptr->type_p);
-		t = (unsigned char *)&type;
-		if (*t == 0)
-			type = ntohs(type);
-		dptr->type = type;
-		t = NULL;
-	}
+/*
+ * Get the values in the correct endianness.
+ */
+	dptr->type = G16BIT_VAL(*((uint16_t *)dptr->type_p), endian);
+	dptr->len = G32BIT_VAL(*((uint32_t *)dptr->len_p), endian);
+	dptr->offset = G32BIT_VAL(*((uint32_t *)dptr->offset_p), endian);
 
 	if (dptr->offset >= (lim - file->map) || dptr->offset >= (file->map_end - file->map))
 	{
@@ -361,55 +372,40 @@ get_data_offset(file_t *file, datum_t *dptr, char *str, size_t slen, uint16_t ty
 			case TYPE_BYTE:
 			case TYPE_ASCII:
 			case TYPE_COMMENT:
-			dptr->data_end = (void *)(p + dptr->len);
-			break;
+
+				dptr->data_end = (void *)(p + dptr->len);
+				break;
+
 			case TYPE_SHORT:
-			dptr->data_end = (void *)(p + (dptr->len * 2));
-			break;
+
+				dptr->data_end = (void *)(p + (dptr->len * 2));
+				break;
+
 			case TYPE_SRATIONAL:
 			case TYPE_RATIONAL:
-			dptr->data_end = (void *)(p + (dptr->len * (sizeof(unsigned int) * 2)));
-			break;
+
+				dptr->data_end = (void *)(p + (dptr->len * (sizeof(unsigned int) * 2)));
+				break;
+
 			default:
-			dptr->data_end = (void *)(p + dptr->len);
+				dptr->data_end = (void *)(p + dptr->len);
 		}
+
+		assert(dptr->data_end < file->map_end);
 
 		p = NULL;
 		return (void *)dptr;
 	}
 }
 
-/**
- * The compiler will complain about this being defined but unused
- * if we are on a little endian machine since the preprocessor
- * will not make use of this function (see get_tag() func), so
- * use the unused attribute.
- */
-static uint32_t
-__attribute__((unused)) __reverse_bytes32(uint32_t val)
-{
-	uint8_t t;
-	uint8_t *p = (uint8_t *)&val;
-
-	t = p[3];
-	p[3] = p[0];
-	p[0] = t;
-
-	t = p[2];
-	p[2] = p[1];
-	p[1] = t;
-
-	return *((uint32_t *)p);
-}
-
 static char __tag[2];
 
 /**
- * Tags have been encoded in big endian format.
- *
- * On big endian machines, both ntohs() and htons()
- * won't flip the bytes. We therefore need to manually
- * flip them if we are big endian.
+ * We don't use the GXXBIT_VAL() functions here because
+ * regardless of which processor we are running on, the
+ * exif tags have been hardcoded in big endian. We just
+ * need to flip the tag around if the exif data itself
+ * is encoded in little endian.
  */
 static char *get_tag(char *t, int e)
 {
@@ -420,8 +416,12 @@ static char *get_tag(char *t, int e)
 	val = *((uint16_t *)t);
 	if (!e)
 	{
+/*
+ * If the processor is big endian, we need to use our own
+ * reversing function since both ntohs/htons will do nothing.
+ */
 #if __BYTE_ORDER == __BIG_ENDIAN
-		val = __reverse_bytes32(val);
+		val = __reverse_bytes16(val);
 #else
 		val = ntohs(val);
 #endif
@@ -492,37 +492,72 @@ fail:
 	return NULL;	
 }
 
-/**
- * Extract the exif data corresponding to the
- * flags in EXIF_FLAGS. GPS data needs to be
- * treated specially and cannot be done in the
- * for-loop.
- *
- * @param file Structure with pointer to mapped file contents
- * @param endian Non-zero means the exif-data is big-endian
- */
-int
-extract_data(file_t *file, int endian)
+static data_array_t *new_data_array(void) __unused;
+
+static data_array_t *
+new_data_array(void)
 {
+	data_array_t *arr = malloc(sizeof(data_array_t));
+
+	if (NULL == arr)
+		return NULL;
+
+	memset(arr, 0, sizeof(*arr));
+	return arr;
+}
+
+/**
+ * Extend the datum_t array in ARR and memcpy
+ * data in D. Do not free D as it is a stack
+ * address passed from the caller.
+ */
+static void
+__unused add_datum(data_array_t *arr, datum_t *d)
+{
+	assert(arr);
+	assert(d);
+
+	if (NULL == arr->data)
+		arr->data = calloc(1, sizeof(datum_t));
+	else
+		arr->data = realloc(arr->data, (arr->nr+1) * sizeof(datum_t));
+
+	assert(arr->data);
+
+	memcpy(&arr->data[arr->nr], d, sizeof(datum_t));
+	++arr->nr;
+
+	return;
+}
+
+//static data_array_t *sensitive = NULL;
+
+int
+extract_sensitive(file_t *file, int endian)
+{
+	assert(file);
+
 	int i;
 	datum_t datum;
 	void *p = NULL;
 	exif_flag_t *flag = NULL;
 	char *tag;
 
-	assert(file);
+	//sensitive = new_data_array();
+	//assert(sensitive);
 
-	for (i = 0; i < NR_DATA; ++i)
+	for (i = 0; i < NR_SENSITIVE; ++i)
 	{
 		clear_struct(&datum);
 
-		flag = &EXIF_FLAGS[i];
-
+		flag = &SENSITIVE_DATA[i];
 	/*
 	 * Gets the tag in the correct endianness.
 	 */
 		tag = get_tag(flag->flag, endian);
 		p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
+
+		//add_datum(sensitive, &datum);
 
 		if (!p || datum.type != flag->type || !datum.len)
 			continue;
@@ -531,20 +566,93 @@ extract_data(file_t *file, int endian)
 #define END		"\e[m"
 		fprintf(stdout, "%*s: %s%s%s\n",
 			(int)NAME_WIDTH, flag->name,
-			FLAGS & WIPE_ALL ? STRIKETHROUGH : "",
+			FLAGS & WIPE_SENSITIVE ? STRIKETHROUGH : "",
 			(char *)datum.data_start,
-			FLAGS & WIPE_ALL ? END : "");
+			FLAGS & WIPE_SENSITIVE ? END : "");
 
-		if (FLAGS & WIPE_ALL)
+		if (FLAGS & WIPE_SENSITIVE)
 			zero_data(file, &datum);
+
+		++count;
 	}
 
+/*
+ * Get the GPS Version ID data.
+ */
+	flag = &GPS_FLAGS[GPS_VERSION_ID];
+	tag = get_tag(flag->flag, endian);
+
+	p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
+	if (p && datum.type == flag->type && datum.len > 0)
+	{
+		char *ptr = (char *)datum.data_start;
+		char *t;
+		char *e = ptr + 4;
+		static char version_id[64];
+
+		t = version_id;
+
+		while (ptr < e)
+		{
+			*t++ = (*ptr++ + '0');
+			*t++ = '.';
+		}
+
+		*--t = 0;
+
+		fprintf(stdout, "%*s: %s%s%s\n",
+			(int)NAME_WIDTH, flag->name,
+			FLAGS & WIPE_SENSITIVE ? STRIKETHROUGH : "",
+			version_id,
+			FLAGS & WIPE_SENSITIVE ? END : "");
+
+		if (FLAGS & WIPE_SENSITIVE)
+			zero_data(file, &datum);
+
+		++count;
+	}
+
+/*
+ * Get the GPS Datestamp data.
+ */
+	flag = &GPS_FLAGS[GPS_DATESTAMP];
+	tag = get_tag(flag->flag, endian);
+
+	p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
+	if (p && datum.type == flag->type && datum.len > 0)
+	{
+		fprintf(stdout, "%*s: %s%s%s\n",
+			(int)NAME_WIDTH, flag->name,
+			FLAGS & WIPE_SENSITIVE ? STRIKETHROUGH : "",
+			(char *)datum.data_start,
+			FLAGS & WIPE_SENSITIVE ? END : "");
+
+		if (FLAGS & WIPE_SENSITIVE)
+			zero_data(file, &datum);
+
+		++count;
+	}
+
+/*
+ * Extract the actual GPS data, including the letters indicating
+ * whether latitude is North or South and longitude East or West.
+ *
+ * Location data is coded as 3 RATIONAL types for degrees,
+ * arc-minutes and arc-seconds. A RATIONAL type is two uint32_t
+ * values, first being the numerator and the second the denominator:
+ *
+ *     degrees        arc-minutes     arc-seconds
+ * [ num ][ denom ][ num ][ denom ][ num ][ denom ]
+ *
+ * for a total of 24 bytes of data.
+ */
 	char lat_NS[2];
 	char long_EW[2];
+	//datum_t latNS, lngEW, dlat, dlng;
 
 	flag = &GPS_FLAGS[GPS_LATITUDE_LETTER];
 	tag = get_tag(flag->flag, endian);
-	clear_struct(&datum);
+
 	p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
 
 /*
@@ -558,7 +666,7 @@ extract_data(file_t *file, int endian)
 
 	flag = &GPS_FLAGS[GPS_LONGITUDE_LETTER];
 	tag = get_tag(flag->flag, endian);
-	clear_struct(&datum);
+
 	p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
 
 	if (!p || datum.type != flag->type)
@@ -569,7 +677,7 @@ extract_data(file_t *file, int endian)
 
 	flag = &GPS_FLAGS[GPS_LATITUDE];
 	tag = get_tag(flag->flag, endian);
-	clear_struct(&datum);
+
 	p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
 
 	if (!p || datum.type != flag->type)
@@ -591,7 +699,7 @@ extract_data(file_t *file, int endian)
 
 	flag = &GPS_FLAGS[GPS_LONGITUDE];
 	tag = get_tag(flag->flag, endian);
-	clear_struct(&datum);
+
 	p = get_data_offset(file, &datum, tag, 2, flag->type, endian);
 	if (!p || datum.type != flag->type)
 		goto end;
@@ -606,20 +714,122 @@ extract_data(file_t *file, int endian)
 
 	free(vals);
 
+	//add_datum(sensitive, &latNS);
+	//add_datum(sensitive, &lngEW);
+	//add_datum(sensitive, &dlat);
+	//add_datum(sensitive, &dlng);
+
+	count += 4;
+
 	fprintf(stderr,
 		"%*s: %sLat %.2lf°%.2lf'%.2lf″ %s, Long %.2lf°%.2lf'%.2lf″ %s%s\n",
 		(int)NAME_WIDTH, "Location",
-		FLAGS & WIPE_ALL ? STRIKETHROUGH : "",
+		FLAGS & WIPE_SENSITIVE ? STRIKETHROUGH : "",
 		latd, latam, latas, lat_NS, lngd, lngam, lngas, long_EW,
-		FLAGS & WIPE_ALL ? END : "");
+		FLAGS & WIPE_SENSITIVE ? END : "");
 
-	if (FLAGS & WIPE_ALL)
+	if (FLAGS & WIPE_SENSITIVE)
 		zero_data(file, &datum);
-
 end:
-	return 0;
+	return count;
 }
 
+static void
+__unused show_sensitive(data_array_t *arr)
+{
+	assert(arr);
+
+	int i;
+	int nr = arr->nr;
+	datum_t *d = NULL;
+	void *ptr;
+	double dbl;
+
+	for (i = 0; i < nr; ++i)
+	{
+		d = &arr->data[i];
+
+		switch(d->type)
+		{
+			case TYPE_ASCII:
+			case TYPE_BYTE:
+			case TYPE_COMMENT:
+
+				fprintf(stdout, "%s\n", (char *)d->data_start);
+
+				break;
+
+			case TYPE_RATIONAL:
+
+				ptr = d->data_start;
+				while (ptr < d->data_end)
+				{
+					dbl = parse_rational(&ptr);
+					fprintf(stdout, "%.2lf ", dbl);
+				}
+
+				fprintf(stdout, "\n");
+
+				break;
+
+			default:
+				break;
+		}
+	}
+}
+
+static void
+__unused free_data_array(data_array_t *arr)
+{
+	assert(arr);
+
+	if (NULL == arr->data)
+		return;
+
+	free(arr->data);
+	free(arr);
+
+	return;
+}
+
+static void
+__unused wipe_sensitive(file_t *file, data_array_t *arr)
+{
+	assert(file);
+	assert(arr);
+
+	int i;
+	int nr = arr->nr;
+	datum_t *d;
+
+	for (i = 0; i < nr; ++i)
+	{
+		d = &arr->data[i];
+		zero_data(file, d);
+	}
+
+	return;
+}
+
+/**
+ * @param file Structure with pointer to mapped file contents
+ * @param endian Non-zero means the exif-data is big-endian
+ */
+int
+extract_data(file_t *file, int endian)
+{
+	assert(file);
+
+	return extract_sensitive(file, endian);
+	//show_sensitive(sensitive);
+
+	//if (FLAGS & WIPE_SENSITIVE)
+	//	wipe_sensitive(file, sensitive);
+
+	//free_data_array(sensitive);
+}
+
+#if 0
 int
 get_date_time(file_t *file, int endian)
 {
@@ -1242,6 +1452,7 @@ get_miscellaneous_data(file_t *file, int endian)
 
 	return count;
 }
+#endif
 
 void *
 get_limit(file_t *file)
